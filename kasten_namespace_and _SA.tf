00ba8c6e6c3964eb787a584dfc41c82d70ad3f76
Pablo Linares <plinares@at.com.uy>
2025-12-12 12:16:57 -0300

    Fixed provider on kasten namespace and SA

--- kasten_namespace_and _SA.tf
+++ kasten_namespace_and _SA.tf
@@ -1,10 +1,13 @@
 resource "kubernetes_namespace" "kasten_io" {
+  provider = kubernetes.eks
   metadata {
     name = "kasten-io"
   }
+  depends_on = [aws_eks_cluster.main]
 }
 
 resource "kubernetes_service_account" "kasten_sa" {
+  provider = kubernetes.eks
   metadata {
     name      = "kasten-sa"
     namespace = kubernetes_namespace.kasten_io.metadata[0].name
@@ -12,4 +15,5 @@ resource "kubernetes_service_account" "kasten_sa" {
       "eks.amazonaws.com/role-arn" = aws_iam_role.kasten_role.arn
     }
   }
+  depends_on = [kubernetes_namespace.kasten_io]
 }
