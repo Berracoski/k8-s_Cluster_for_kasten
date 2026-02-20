c21a5fdc6f1ef312248a7602b7bd868bf8f5a5ff
Pablo Linares <plinares@at.com.uy>
2026-02-20 17:43:24 -0300

    After destroy

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -11,5 +11,26 @@ resource "aws_eks_cluster" "main" {
     endpoint_private_access = true
     endpoint_public_access  = true
   }
+  # access_config {
+  #   authentication_mode                         = "API_AND_CONFIG_MAP"
+  #   bootstrap_cluster_creator_admin_permissions = true
+  # }
 }
 
+# # 1. Create the base access entry (Remove the kubernetes_groups line)
+# resource "aws_eks_access_entry" "magarcia" {
+#   cluster_name      = aws_eks_cluster.main.name
+#   principal_arn     = "arn:aws:iam::528775625753:user/magarcia"
+#   type              = "STANDARD"
+# }
+
+# # 2. Grant Admin rights via an EKS Access Policy
+# resource "aws_eks_access_policy_association" "magarcia_admin" {
+#   cluster_name  = aws_eks_cluster.main.name
+#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#   principal_arn = aws_eks_access_entry.magarcia.principal_arn
+
+#   access_scope {
+#     type = "cluster"
+#   }
+# }
--- node-group.tf
+++ node-group.tf
@@ -5,9 +5,9 @@ resource "aws_eks_node_group" "default" {
   subnet_ids      = aws_subnet.private[*].id
 
   scaling_config {
-    desired_size = 3
+    desired_size = 0
     max_size     = 3
-    min_size     = 1
+    min_size     = 0
   }
 
   instance_types = ["t3.medium"]
@@ -15,6 +15,44 @@ resource "aws_eks_node_group" "default" {
 
   labels = {
     role = "worker"
+    Cliente = "Claro"
+    Servicio   = "Curso_Claro"
   }
 
+}
+
+
+resource "aws_eks_node_group" "spot_nodes" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "spot-node-group"
+  node_role_arn   = aws_iam_role.eks_node.arn
+  subnet_ids      = aws_subnet.private[*].id
+
+  capacity_type = "SPOT"
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 10 # Increase max to allow for room during Spot rebalancing
+    min_size     = 1
+  }
+
+  # DIVERSIFIED LIST:
+  instance_types = [
+    "t3.medium", 
+    "t3a.medium", 
+    "t2.medium",
+    "t2.large",
+    "c5.large", 
+    "c5a.large", 
+    "c4.large"
+  ]
+
+  ami_type = "AL2023_x86_64_STANDARD"
+
+  labels = {
+    role         = "worker"
+    capacityType = "SPOT"
+    Cliente = "Claro"
+    Servicio   = "Curso_Claro"
+  }
 }
